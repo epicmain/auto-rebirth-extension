f2d4827280d5dc7af49d77610ddd1e080ab6c8a5
zhen2004ming <85509340+zhen2004ming@users.noreply.github.com>
2024-09-03 22:33:56 +0800

    Update auto rebirth.lua

--- auto rebirth.lua
+++ auto rebirth.lua
@@ -1,7 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/waitForGameLoad.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zhen2004ming/auto-rebirth-extension/main/auto%20rebirth%20extension.lua"))()
 
-print("STARTED MONG REBIRTH2")
+print("STARTED MONG REBIRTH3")
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LocalPlayer = game:GetService("Players").LocalPlayer
