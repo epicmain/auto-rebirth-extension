1a51963f7af750088b25a62b873f517652f92fd5
zhen2004ming <85509340+zhen2004ming@users.noreply.github.com>
2024-09-08 07:43:25 +0800

    Update auto rebirth.lua

--- auto rebirth.lua
+++ auto rebirth.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/waitForGameLoad.lua"))()
-print("rebirth started.")
+print("Updated hatching.")
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Library = ReplicatedStorage:WaitForChild("Library")
@@ -307,12 +307,9 @@ end
 
 local function teleportToMaxZone()
     print("in teleportToMaxZone()")
-    bestEgg = clientSaveGet.MaximumAvailableEgg
-    eggData = require(Library.Util.EggsUtil).GetByNumber(bestEgg) -- gets eggData.name, .eggNumber
 
     local zoneName, maxZoneData = zoneCmds.GetMaxOwnedZone()
-    print(zoneName)
-    print(currentZone)
+    print("Teleporting to: ", zoneName)
     while currentZone == zoneName do
         zoneName, maxZoneData = zoneCmds.GetMaxOwnedZone()
         task.wait()
@@ -641,6 +638,10 @@ end
 
 
 local function teleportAndHatch()
+    bestEgg = clientSaveGet.MaximumAvailableEgg
+    eggData = require(Library.Util.EggsUtil).GetByNumber(bestEgg) -- gets eggData.name, .eggNumber
+    print("New obtained eggData: ", eggData.name, " (", eggData.eggNumber, ")")
+    
     originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
     -- Teleport to Best Egg
     for _, v in pairs(game:GetService("Workspace").__THINGS.Eggs.Main:GetChildren()) do
@@ -895,6 +896,6 @@ task.spawn(function()
     end
 end)
 
-teleportToMaxZone()
+teleportToMaxZone()  - delay before starting parallel functions.
 
 
